e6c24bbeef9be4dbc0c66a33a898d0b970443d26
moumou <r12944042@ntu.edu.tw>
2025-02-23 03:17:31 +0800

    test: update test.sh

--- rdma_client/test/test.sh
+++ rdma_client/test/test.sh
@@ -4,3 +4,10 @@ client_num=2
 
 make --no-print-directory -C .. clean
 make --no-print-directory -C .. TEST_MULTI_STREAM=$client_num
+
+rm -rf ../log/*
+
+for (( i=1; i<=client_num; i++ ))
+do
+    ../client > ../log/client${i}.log 2>&1 &
+done
