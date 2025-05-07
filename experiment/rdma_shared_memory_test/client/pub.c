2b2d1c2fc6e08e669a61f53c9ffa72f21448ad6d
moumou <r12944042@ntu.edu.tw>
2025-05-07 23:38:31 +0800

    feat: add /experiment/rdma_shared_memory_test/client/pub.c

---
+++ experiment/rdma_shared_memory_test/client/pub.c
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define RDMA_BUFFER_SIZE            ((1UL) << 20)
+
+/* shared memory info */
+#define SHM_NAME                    "/rdma_shm1"
+
+int main(int argc, char* argv[])
+{
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) { 
+        perror("shm_open"); 
+        return 1; 
+    }
+
+    char *buffer = mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (buffer == MAP_FAILED) { 
+        perror("mmap"); 
+        return 1;
+    }
+    close(fd);
+
+    char cnt = 0;
+    for (int i = 0; i < RDMA_BUFFER_SIZE; i++) {
+        buffer[i] = cnt;
+        cnt++;
+    }
+
+    munmap(buf, RDMA_BUFFER_SIZE);
+    shm_unlink(SHM_NAME);
+    return 0;
+}
